ef77e7c5517d3fe4c888e705c413046e24c68091
Rahulgithub <77477603+UserGitHub24@users.noreply.github.com>
2021-01-21 16:39:34 +0530

    Add files via upload

---
+++ public/customActivity.js
@@ -0,0 +1,77 @@
+'use strict';
+
+define(function (require) {
+	var Postmonger = require('postmonger');
+	var connection = new Postmonger.Session();
+	var payload = {};
+	var steps = [
+		{'key': 'step1', 'label': 'Step 1'}
+	];
+	var currentStep = steps[0].key;
+
+	$(window).ready(function () {
+		connection.trigger('ready');
+	});
+
+	function initialize (data) {
+		if (data) {
+			payload = data;
+		}
+	}
+
+	function onClickedNext () {
+		if (currentStep.key === 'step1') {
+			save();
+		} else {
+			connection.trigger('nextStep');
+		}
+	}
+
+	function onClickedBack () {
+		connection.trigger('prevStep');
+	}
+
+	function onGotoStep (step) {
+		showStep(step);
+		connection.trigger('ready');
+	}
+
+	function showStep (step, stepIndex) {
+		if (stepIndex && !step) {
+			step = steps[stepIndex - 1];
+		}
+
+		currentStep = step;
+
+		$('.step').hide();
+
+		switch 	(currentStep.key) {
+		case 'step1':
+			$('#step1').show();
+			$('#step1 input').focus();
+			break;
+		}
+	}
+
+	function save () {
+		var step1 = $('#statusCode').val();
+
+		payload['arguments'] = payload['arguments'] || {};
+		payload['arguments'].execute = payload['arguments'].execute || {};
+		payload['arguments'].execute.inArguments = [{
+			'serviceCloudId': '{{Event.' + step1 + '.\"Contact:Id\"}}'
+		}];
+
+		payload['metaData'] = payload['metaData'] || {};
+		payload['metaData'].isConfigured = true;
+
+		console.log(JSON.stringify(payload));
+
+		connection.trigger('updateActivity', payload);
+	}
+
+	connection.on('initActivity', initialize);
+	connection.on('clickedNext', onClickedNext);
+	connection.on('clickedBack', onClickedBack);
+	connection.on('gotoStep', onGotoStep);
+});
