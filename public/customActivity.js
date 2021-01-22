db5e6b139ff73dbe75c2d4b2c71aa65d3dbce124
Rahulgithub <77477603+UserGitHub24@users.noreply.github.com>
2021-01-22 15:03:32 +0530

    Update customActivity.js

--- public/customActivity.js
+++ public/customActivity.js
@@ -54,37 +54,6 @@ define(function (require) {
 		}
 	}
 	
-	
-     var hasInArguments = Boolean(
-	 payload['arguments'] &&
-	 payload['arguments'].execute &&
-	 payload['arguments'].execute.inArguments &&
-	 payload['arguments'].execute.inArguments.length > 0
-	);
-
-	var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
-	
-	console.log(inArguments);
-	
-	$.each(inArguments, function(index, inArgument) {
-	 $.each(inArgument function (key, val) {
-	 
-	 if (key === 'eventdefinitionkey')
-	 {
-	   $('#statusCode').val(val);
-	   
-	 }
-	 
-	 });
-	 
-	});
-
-   connection.trigger('updateButton', {
-   button: 'next',
-   text: 'done',
-   visible: true
-   
-   });
 
 	function save () {
 		var statusCode = $('#statusCode').val();
